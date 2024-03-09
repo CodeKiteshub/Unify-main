e0494ae013491e29c72d6573d89ffcbb53bad466
CodeKiteshub <ak1648475@gmail.com>
2024-03-09 12:55:06 +0530

    Initial commit

---
+++ lib/pages/settings_security/settings_security_view.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/config/setting_keys.dart';
+import 'package:fluffychat/utils/beautify_string_extension.dart';
+import 'package:fluffychat/utils/localized_exception_extension.dart';
+import 'package:fluffychat/utils/platform_infos.dart';
+import 'package:fluffychat/widgets/layouts/max_width_body.dart';
+import 'package:fluffychat/widgets/matrix.dart';
+import 'package:fluffychat/widgets/settings_switch_list_tile.dart';
+import 'settings_security.dart';
+
+class SettingsSecurityView extends StatelessWidget {
+  final SettingsSecurityController controller;
+  const SettingsSecurityView(this.controller, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(L10n.of(context)!.security)),
+      body: ListTileTheme(
+        iconColor: Theme.of(context).colorScheme.onBackground,
+        child: MaxWidthBody(
+          child: FutureBuilder(
+            future: Matrix.of(context)
+                .client
+                .getCapabilities()
+                .timeout(const Duration(seconds: 10)),
+            builder: (context, snapshot) {
+              final capabilities = snapshot.data;
+              final error = snapshot.error;
+              if (error == null && capabilities == null) {
+                return const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: CircularProgressIndicator.adaptive(strokeWidth: 2),
+                  ),
+                );
+              }
+              return Column(
+                children: [
+                  if (error != null)
+                    ListTile(
+                      leading: const Icon(
+                        Icons.warning_outlined,
+                        color: Colors.orange,
+                      ),
+                      title: Text(
+                        error.toLocalizedString(context),
+                        style: const TextStyle(color: Colors.orange),
+                      ),
+                    ),
+                  if (capabilities?.mChangePassword?.enabled == true ||
+                      error != null) ...[
+                    ListTile(
+                      leading: const Icon(Icons.key_outlined),
+                      trailing: error != null
+                          ? null
+                          : const Icon(Icons.chevron_right_outlined),
+                      title: Text(
+                        L10n.of(context)!.changePassword,
+                        style: TextStyle(
+                          decoration:
+                              error == null ? null : TextDecoration.lineThrough,
+                        ),
+                      ),
+                      onTap: error != null
+                          ? null
+                          : () =>
+                              context.go('/rooms/settings/security/password'),
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.mail_outlined),
+                      trailing: error != null
+                          ? null
+                          : const Icon(Icons.chevron_right_outlined),
+                      title: Text(
+                        L10n.of(context)!.passwordRecovery,
+                        style: TextStyle(
+                          decoration:
+                              error == null ? null : TextDecoration.lineThrough,
+                        ),
+                      ),
+                      onTap: error != null
+                          ? null
+                          : () => context.go('/rooms/settings/security/3pid'),
+                    ),
+                  ],
+                  ListTile(
+                    leading: const Icon(Icons.block_outlined),
+                    trailing: const Icon(Icons.chevron_right_outlined),
+                    title: Text(L10n.of(context)!.blockedUsers),
+                    onTap: () =>
+                        context.go('/rooms/settings/security/ignorelist'),
+                  ),
+                  if (Matrix.of(context).client.encryption != null) ...{
+                    if (PlatformInfos.isMobile)
+                      ListTile(
+                        leading: const Icon(Icons.lock_outlined),
+                        trailing: const Icon(Icons.chevron_right_outlined),
+                        title: Text(L10n.of(context)!.appLock),
+                        onTap: controller.setAppLockAction,
+                      ),
+                  },
+                  const Divider(height: 1),
+                  ListTile(
+                    leading: const Icon(Icons.tap_and_play),
+                    title: Text(
+                      L10n.of(context)!.dehydrate,
+                      style: const TextStyle(color: Colors.red),
+                    ),
+                    onTap: controller.dehydrateAction,
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.delete_outlined),
+                    title: Text(
+                      L10n.of(context)!.deleteAccount,
+                      style: const TextStyle(color: Colors.red),
+                    ),
+                    onTap: controller.deleteAccountAction,
+                  ),
+                  ListTile(
+                    title: Text(L10n.of(context)!.yourPublicKey),
+                    subtitle: SelectableText(
+                      Matrix.of(context).client.fingerprintKey.beautified,
+                      style: const TextStyle(fontFamily: 'monospace'),
+                    ),
+                    leading: const Icon(Icons.vpn_key_outlined),
+                  ),
+                  const Divider(height: 1),
+                  SettingsSwitchListTile.adaptive(
+                    title: L10n.of(context)!.sendTypingNotifications,
+                    subtitle:
+                        L10n.of(context)!.sendTypingNotificationsDescription,
+                    onChanged: (b) => AppConfig.sendTypingNotifications = b,
+                    storeKey: SettingKeys.sendTypingNotifications,
+                    defaultValue: AppConfig.sendTypingNotifications,
+                  ),
+                  SettingsSwitchListTile.adaptive(
+                    title: L10n.of(context)!.sendReadReceipts,
+                    subtitle: L10n.of(context)!.sendReadReceiptsDescription,
+                    onChanged: (b) => AppConfig.sendPublicReadReceipts = b,
+                    storeKey: SettingKeys.sendPublicReadReceipts,
+                    defaultValue: AppConfig.sendPublicReadReceipts,
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/settings_style/settings_style_view.dart
@@ -0,0 +1,290 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+import 'package:fluffychat/config/themes.dart';
+import 'package:fluffychat/utils/account_config.dart';
+import 'package:fluffychat/widgets/avatar.dart';
+import 'package:fluffychat/widgets/layouts/max_width_body.dart';
+import 'package:fluffychat/widgets/matrix.dart';
+import 'package:fluffychat/widgets/mxc_image.dart';
+import '../../config/app_config.dart';
+import 'settings_style.dart';
+
+class SettingsStyleView extends StatelessWidget {
+  final SettingsStyleController controller;
+
+  const SettingsStyleView(this.controller, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const colorPickerSize = 32.0;
+    final client = Matrix.of(context).client;
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Center(child: BackButton()),
+        title: Text(L10n.of(context)!.changeTheme),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: MaxWidthBody(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                L10n.of(context)!.setColorTheme,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: colorPickerSize + 24,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: SettingsStyleController.customColors
+                    .map(
+                      (color) => Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(colorPickerSize),
+                          onTap: () => controller.setChatColor(color),
+                          child: color == null
+                              ? Material(
+                                  elevation: 6,
+                                  shadowColor: AppConfig.colorSchemeSeed,
+                                  borderRadius:
+                                      BorderRadius.circular(colorPickerSize),
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(
+                                        colorPickerSize,
+                                      ),
+                                      gradient: FluffyThemes.backgroundGradient(
+                                        context,
+                                        255,
+                                      ),
+                                    ),
+                                    child: SizedBox(
+                                      height: colorPickerSize,
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 8.0,
+                                        ),
+                                        child: Center(
+                                          child: Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              if (controller.currentColor ==
+                                                  null)
+                                                Padding(
+                                                  padding:
+                                                      const EdgeInsets.only(
+                                                    right: 8.0,
+                                                  ),
+                                                  child: Icon(
+                                                    Icons.check,
+                                                    size: 16,
+                                                    color: Theme.of(context)
+                                                        .colorScheme
+                                                        .onBackground,
+                                                  ),
+                                                ),
+                                              Text(
+                                                L10n.of(context)!.systemTheme,
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .onBackground,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              : Material(
+                                  color: color,
+                                  elevation: 6,
+                                  borderRadius:
+                                      BorderRadius.circular(colorPickerSize),
+                                  child: SizedBox(
+                                    width: colorPickerSize,
+                                    height: colorPickerSize,
+                                    child: controller.currentColor == color
+                                        ? const Center(
+                                            child: Icon(
+                                              Icons.check,
+                                              size: 16,
+                                              color: Colors.white,
+                                            ),
+                                          )
+                                        : null,
+                                  ),
+                                ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Divider(height: 1),
+            ListTile(
+              title: Text(
+                L10n.of(context)!.setTheme,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            RadioListTile<ThemeMode>(
+              groupValue: controller.currentTheme,
+              value: ThemeMode.system,
+              title: Text(L10n.of(context)!.systemTheme),
+              onChanged: controller.switchTheme,
+            ),
+            RadioListTile<ThemeMode>(
+              groupValue: controller.currentTheme,
+              value: ThemeMode.light,
+              title: Text(L10n.of(context)!.lightTheme),
+              onChanged: controller.switchTheme,
+            ),
+            RadioListTile<ThemeMode>(
+              groupValue: controller.currentTheme,
+              value: ThemeMode.dark,
+              title: Text(L10n.of(context)!.darkTheme),
+              onChanged: controller.switchTheme,
+            ),
+            const Divider(height: 1),
+            ListTile(
+              title: Text(
+                L10n.of(context)!.messagesStyle,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            StreamBuilder(
+              stream: client.onAccountData.stream.where(
+                (data) =>
+                    data.type ==
+                    ApplicationAccountConfigExtension.accountDataKey,
+              ),
+              builder: (context, snapshot) {
+                final accountConfig = client.applicationAccountConfig;
+                final wallpaperOpacity = accountConfig.wallpaperOpacity ?? 1;
+                final wallpaperOpacityIsDefault = wallpaperOpacity == 1;
+
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    AnimatedContainer(
+                      duration: FluffyThemes.animationDuration,
+                      curve: FluffyThemes.animationCurve,
+                      alignment: Alignment.centerLeft,
+                      decoration: const BoxDecoration(),
+                      clipBehavior: Clip.hardEdge,
+                      child: Stack(
+                        children: [
+                          if (accountConfig.wallpaperUrl != null)
+                            Opacity(
+                              opacity: wallpaperOpacity,
+                              child: MxcImage(
+                                uri: accountConfig.wallpaperUrl,
+                                fit: BoxFit.cover,
+                                isThumbnail: true,
+                                width: FluffyThemes.columnWidth * 2,
+                                height: 156,
+                              ),
+                            ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: 12 + 12 + Avatar.defaultSize,
+                              right: 12,
+                              top: accountConfig.wallpaperUrl == null ? 0 : 12,
+                              bottom: 12,
+                            ),
+                            child: Material(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                              borderRadius: BorderRadius.circular(
+                                AppConfig.borderRadius,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 8,
+                                ),
+                                child: Text(
+                                  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor',
+                                  style: TextStyle(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onPrimaryContainer,
+                                    fontSize: AppConfig.messageFontSize *
+                                        AppConfig.fontSizeFactor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(L10n.of(context)!.wallpaper),
+                      leading: const Icon(Icons.photo_outlined),
+                      trailing: accountConfig.wallpaperUrl == null
+                          ? null
+                          : IconButton(
+                              icon: const Icon(Icons.delete_outlined),
+                              color: Theme.of(context).colorScheme.error,
+                              onPressed: controller.deleteChatWallpaper,
+                            ),
+                      onTap: controller.setWallpaper,
+                    ),
+                    AnimatedSize(
+                      duration: FluffyThemes.animationDuration,
+                      curve: FluffyThemes.animationCurve,
+                      child: accountConfig.wallpaperUrl != null
+                          ? SwitchListTile.adaptive(
+                              title: Text(L10n.of(context)!.transparent),
+                              secondary: const Icon(Icons.blur_linear_outlined),
+                              value: !wallpaperOpacityIsDefault,
+                              onChanged: (_) =>
+                                  controller.setChatWallpaperOpacity(
+                                wallpaperOpacityIsDefault ? 0.4 : 1,
+                              ),
+                            )
+                          : null,
+                    ),
+                  ],
+                );
+              },
+            ),
+            ListTile(
+              title: Text(L10n.of(context)!.fontSize),
+              trailing: Text('× ${AppConfig.fontSizeFactor}'),
+            ),
+            Slider.adaptive(
+              min: 0.5,
+              max: 2.5,
+              divisions: 20,
+              value: AppConfig.fontSizeFactor,
+              semanticFormatterCallback: (d) => d.toString(),
+              onChanged: controller.changeFontSizeFactor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
